23ee883a63345b5a78a90603be1c15d24fae426a
Stephanie Baron <stephaniebaron12@gmail.com>
2017-10-12 10:03:31 -0400

    Added a pet to the JSON file by app.post & fs.readFile

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,21 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/pets', function (req, res){
+  fs.readFile('pets.json', function(err, data){
+    var pets = JSON.parse(data);
+    var pet = req.body;
+    pets.push(pet);
+    res.send(pets);
+  })
+  // console.log(req.body.name);
+  // res.send(req.body.name + req.body.age + req.body.kind);
+})
+
 app.get('/pets', function(req, res){
   fs.readFile('pets.json', 'utf8', function(err, data){
     if(err){
@@ -22,4 +37,6 @@ app.get('/pets/:index', function(req,res){
   })
 })
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log('Listening at port 3000');
+});
